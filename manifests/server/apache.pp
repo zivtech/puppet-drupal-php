e8ae85f739520676919c16d7edb5e79b438856b3
Howard Tyson <howard@howardtyson.com>
2015-02-18 17:17:01 -0500

    Fixing listen and vhost handling.

--- manifests/server/apache.pp
+++ manifests/server/apache.pp
@@ -1,25 +1,17 @@
 class drupal_php::server::apache (
-    $server_port = $drupal_php::params::server_port
+    $manage_server_listen = true,
+    $server_port = $drupal_php::params::server_port,
+    $mpm_module = 'prefork',
+    $server_default_vhost = true
   ) {
-  # class { 'apache':
-  #   default_mods   => false,
-  #   mpm_module     => 'prefork',
-  #   service_enable => false,
-  #   # Do not purge configs because it will remove any vhosts we put in palce.
-  #   purge_configs => false,
-  # }
-  # include ::apache
 
-  # Alias to prevent a conflict with this module.
-  # class { 'apache':
-  #   alias          => 'apache2',
-  #   default_mods  => false,
-  #   mpm_module    => false,
-  #   purge_configs => false,
-  # }
+
   class { '::apache':
-    default_mods => [],
+    default_mods   => [],
+    mpm_module     => $mpm_module,
+    default_vhost => false,
   }
+
   apache::mod{ 'actions': }
   # apache::mod { 'alias': }
   apache::mod { 'auth_basic': }
@@ -46,7 +38,34 @@ class drupal_php::server::apache (
   apache::mod { 'suexec': }
   apache::mod { 'xsendfile': }
 
-  apache::listen { $server_port: }
+  
+  $vhost_ensure = $server_default_vhost ? {
+    true  => 'present',
+    false => 'absent',
+  }
+  ::apache::vhost { '000-default':
+    ensure          => $vhost_ensure,
+    port            => $server_port,
+    docroot         => $::apache::docroot,
+    scriptalias     => $::apache::scriptalias,
+    serveradmin     => $::apache::serveradmin,
+    access_log_file => $::apache::access_log_file,
+    priority        => '15',
+    ip              => $::apache::ip,
+    logroot_mode    => $::apache::logroot_mode,
+  }
+
+  if ($manage_server_listen) {
+    # This appears by default, if the port is not 80 we should remove it.
+    if ($server_port != 80) {
+      concat::fragment { "Listen 80":
+        ensure  => absent,
+        target  => $::apache::ports_file,
+        content => template('apache/listen.erb'),
+      }
+    }
+    apache::listen { $server_port: }
+  }
 
   include php::apache
 }
