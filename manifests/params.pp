71db035e6ed7598eb57b4ccb595feb6b61aa32b5
Jody Hamilton <jody@zivtech.com>
2015-12-23 13:18:51 -0500

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,8 +12,7 @@ class drupal_php::params (
 
   $memory_limit_server = '128M'
   $memery_limit_cli = '-1'
-  $max_execution_time_server = 30
-  $max_execution_time_cli = -1
+  $max_execution_time = 30
   $post_max_size = '8M'
   $upload_max_filesize = '200M'
   $error_log_directory = '/var/log/php'
