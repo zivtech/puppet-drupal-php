210904b7309154099d8c648f43fa019cd207ce9d
Matt <matt@zivtech.com>
2017-03-21 17:04:05 -0400

    Fixing syntax errors

--- manifests/fpm.pp
+++ manifests/fpm.pp
@@ -1,10 +1,10 @@
 class drupal_php::fpm (
-  $listen                  = $drupal_php::params::fpm_pool_listen,
-  $pm_start_servers        = $drupal_php::params::fpm_pm_start_servers,
-  $pm_min_spare_servers    = $drupal_php::params::fpm_pm_min_spare_servers,
-  $pm_max_spare_servers:   = $drupal_php::params::fpm_pm_max_spare_servers,
-  $pm_max_children:        = $drupal_php::params::fpm_pm_max_children,
-  $pm_max_requests:        = $drupal_php::params::fpm_pm_max_requests,
+  $listen                 = $drupal_php::params::fpm_pool_listen,
+  $pm_start_servers       = $drupal_php::params::fpm_pm_start_servers,
+  $pm_min_spare_servers   = $drupal_php::params::fpm_pm_min_spare_servers,
+  $pm_max_spare_servers   = $drupal_php::params::fpm_pm_max_spare_servers,
+  $pm_max_children        = $drupal_php::params::fpm_pm_max_children,
+  $pm_max_requests        = $drupal_php::params::fpm_pm_max_requests,
 ) inherits drupal_php::params {
 
   ::php::fpm::pool {'drupal_php':
